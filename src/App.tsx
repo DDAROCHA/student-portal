058ed21ec04c7189477b2d1d01980cda2bc22a20
Diego Da Rocha <ddarocha00@gmail.com>
2026-03-03 08:58:51 -0300

    Conectado-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [message, setMessage] = useState('Connecting to backend...');
+
+  useEffect(() => {
+    fetch('http://localhost:3001/message') //http://34.237.142.7:3001/message
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(() => setMessage('Error connecting to backend'));
+  }, []);
+
   return (
     <div style={{ padding: 40 }}>
       <h1>Student Portal</h1>
-      <p>Connecting to backend...</p>
+      <p>{message}</p>
     </div>
   );
 }
